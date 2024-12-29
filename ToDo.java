2e74be67d105272778777113e61f42c8e0b47628
Nakshatra Tondepu <ntondepu@purdue.edu>
2024-12-29 18:27:30 -0500

    Completed ToDo java class and implemented all the functionalities.

--- ToDo.java
+++ ToDo.java
@@ -30,4 +30,46 @@ public class ToDo{
             }
         } while (choice != 5);
     }
+    public static void addTask(Scanner scanner){
+        System.out.println("Enter task description:");
+        String description = scanner.nextLine();
+        tasks.add(new Task(description));
+        System.out.println("Task added successfully!");
+    }
+
+    public static void removeTask(Scanner scanner){
+        viewTask();
+        System.out.println("Please pick which task you want to remove:");
+        int taskNum = scanner.nextInt();
+        scanner.nextLine();
+        if (taskNum > 0 && taskNum <= tasks.size()) {
+            tasks.remove(taskNum - 1);
+            System.out.println("Task removed.");
+        } else {
+            System.out.println("Invalid task number.");
+        }
+    }
+
+    public static void viewTask(){
+        if (tasks.isEmpty()) {
+            System.out.println("Your to do list is empty!");
+        } else {
+            System.out.println("\nTo-Do List:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
+            }
+        }
+    }
+
+    public static void markTask(Scanner scanner){
+        System.out.println("Which task do you want to mark as completed?");
+        int taskNum = scanner.nextInt();
+        scanner.nextLine();
+        if (taskNum > 0 && taskNum <= tasks.size()) {
+            tasks.get(taskNum - 1).setCompleted(true);
+            System.out.println("Task is marked as completed!");
+        } else {
+            System.out.println("Task number invalid, please enter another.");
+        }
+    }
 }
